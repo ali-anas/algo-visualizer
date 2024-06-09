d1291546b1f5b744d684782a8a7d000d072a8180
ali-anas <anasalialmora@gmail.com>
2024-06-09 15:35:04 +0530

    added Header

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Grid from '../components/Grid/Grid'
-import Controller from '../components/controller/controller'
+import Grid from '../components/Grid/Grid';
+import Controller from '../components/controller/controller';
+import Header from '../components/Header';
 
 const MainLayout = () => {
   return (
     <>
+      <Header />
       <Controller />
       <Grid />
     </>
