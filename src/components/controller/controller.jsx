a60d8ab1f3c5255b71bcb926da0419ce33885eae
ali-anas <anasalialmora@gmail.com>
2024-06-09 15:36:10 +0530

    added controller

--- src/components/controller/controller.jsx
+++ src/components/controller/controller.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { AlgorithmsMap } from '../../lib/algorithms'
 import { useSelector, useDispatch } from 'react-redux'
 import { clearGrid, setStatus, setGrid } from '../../store/pathfinder.slice'
@@ -6,10 +6,11 @@ import { searchPath } from '../../store/searchPath.thunk'
 import { Status } from '../../constants'
 import { highlightPath } from '../../store/highlight.thunk'
 import classes from './controller.module.scss';
-import { useEffect } from 'react'
+import { Play, RefreshCcw , Trash} from 'lucide-react'
 
 
 const Controller = () => {
+  const [selectedAlgo, setSelectedAlgo] = useState('');
   const dispatch = useDispatch();
   const pathfinder = useSelector(state => state.pathfinder);
   const status = useSelector(state => state.pathfinder.status);
@@ -50,12 +51,38 @@ const Controller = () => {
     }
   }, [source, dest])
 
+  async function handleAlgoSelection(e) {
+    const algo = e.target.value;
+    setSelectedAlgo(algo);
+    await executeSearch(AlgorithmsMap[algo].fn, 1);
+  }
+
   console.log("status: " + status);
+  console.log("selected Algo: " + selectedAlgo);
 
   return (
     <section className={classes.controller}>
-      <button onClick={handleVisualize} disabled={status === Status.Searching}>Visualize BFS</button>
-      <button onClick={handleClearGrid} disabled={status === Status.Searching}>Clear Grid</button>
+      <div className={classes.controls}>
+        <button
+          onClick={handleVisualize}
+          disabled={status === Status.Searching || !selectedAlgo}
+          data-tooltip="Play"
+        >
+          <Play stroke={status === Status.Searching || !selectedAlgo ? "#949494" : "#111111"}/>
+        </button>
+        <button
+          onClick={handleClearGrid}
+          disabled={status === Status.Searching}
+          data-tooltip="Clear">
+          <Trash stroke={status === Status.Searching ? "#949494" : "#111111"}/>
+        </button>
+      </div>
+      <select className={classes.algoSelector} value={selectedAlgo} onChange={handleAlgoSelection} disabled={status === Status.Searching}>
+        <option value="" disabled>
+          Select an algorithm
+        </option>
+        {[...Object.entries(AlgorithmsMap)].map(([key, { name }]) => <option key={key} value={key}>{name}</option>)}
+      </select>
     </section>
   )
 }
